26d20eaaf034aefde410992dda632fdc51e2f7b3
Антон <lopatuxin.a@gmail.com>
2022-09-11 22:10:35 +0300

    6. Перевернуть связанный список [#161]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -7,6 +7,7 @@ import java.util.NoSuchElementException;
 
 public class ForwardLinked<T> implements Iterable<T> {
     private Node<T> head;
+    private Node<T> tail;
 
     public boolean isEmpty() {
         return head == null;
@@ -16,17 +17,34 @@ public class ForwardLinked<T> implements Iterable<T> {
         head = new Node<>(value, head);
     }
 
+    public boolean revert() {
+        if (!isEmpty() && head.next != null) {
+            tail = head;
+            Node<T> current = head.next;
+            head.next = null;
+            while (current != null) {
+                Node<T> next = current.next;
+                current.next = head;
+                head = current;
+                current = next;
+            }
+            return true;
+        }
+        return false;
+    }
+
     public void add(T value) {
         Node<T> node = new Node<T>(value, null);
         if (head == null) {
             head = node;
+            tail = node;
             return;
         }
-        Node<T> tail = head;
         while (tail.next != null) {
             tail = tail.next;
         }
         tail.next = node;
+        tail = node;
     }
 
     public T deleteFirst() {
@@ -72,12 +90,5 @@ public class ForwardLinked<T> implements Iterable<T> {
             this.value = value;
             this.next = next;
         }
-    }
-
-    public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-        list.add(1);
-        list.add(2);
-        list.remove(0);
     }
 }
--- src/test/java/ru/job4j/collection/ForwardLinkedTest.java
+++ src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -12,35 +12,29 @@ class ForwardLinkedTest {
     private ForwardLinked<Integer> linked;
 
     @BeforeEach
-    public void init() {
+    void init() {
         linked = new ForwardLinked<>();
+    }
+
+    @Test
+    void whenSize0ThenReturnFalse() {
+        assertThat(linked.revert()).isFalse();
+    }
+
+    @Test
+    void whenSize1ThenReturnFalse() {
+        linked.add(1);
+        assertThat(linked.revert()).isFalse();
+    }
+
+    @Test
+    void whenAddAndRevertTrue() {
         linked.add(1);
         linked.add(2);
         linked.add(3);
         linked.add(4);
-    }
-
-    @Test
-    void whenDeleteFirst() {
-        assertThat(linked.deleteFirst()).isEqualTo(1);
-        assertThat(linked.deleteFirst()).isEqualTo(2);
-        assertThat(linked.deleteFirst()).isEqualTo(3);
-        assertThat(linked.deleteFirst()).isEqualTo(4);
-        assertThatThrownBy(linked.iterator()::next)
-                .isInstanceOf(NoSuchElementException.class);
-    }
-
-    @Test
-    void whenDeleteEmptyLinked() {
-        ForwardLinked<Integer> linked = new ForwardLinked<>();
-        assertThatThrownBy(linked::deleteFirst)
-                .isInstanceOf(NoSuchElementException.class);
-    }
-
-    @Test
-    void whenMultiDelete() {
-        linked.deleteFirst();
-        Iterator<Integer> it = linked.iterator();
-        assertThat(it.next()).isEqualTo(2);
+        assertThat(linked).containsSequence(1, 2, 3, 4);
+        assertThat(linked.revert()).isTrue();
+        assertThat(linked).containsSequence(4, 3, 2, 1);
     }
 }
