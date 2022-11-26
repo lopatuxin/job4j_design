6cb68d491304637d58d0e0670511cc474abb3f40
Антон <lopatuxin.a@gmail.com>
2022-11-26 21:20:48 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -18,8 +18,13 @@ public class EchoServer {
                      )) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String str = in.readLine();
-                    if (str.contains("msg=Bye")) {
-                            server.close();
+                    if (str.contains("msg=Exit")) {
+                        out.write("Bye".getBytes());
+                        server.close();
+                    } else if (str.contains("msg=Hello")) {
+                        out.write("Hello, dear friend.".getBytes());
+                    } else {
+                        out.write(str.split("=")[1].getBytes());
                     }
                     System.out.println(str);
                     out.flush();
